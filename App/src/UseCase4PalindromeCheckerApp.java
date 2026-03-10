b4ef3cedc89de325239e302f085d3a40987b6ca0
Piyush <ps13sinha@gmail.com>
2026-03-10 15:09:17 +0530

    UC4

---
+++ App/src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,42 @@
+// version 1.0
+//author Abc
+//use Case 1: Welcome page
+
+
+import java.util.Scanner;
+
+class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Declare and initialize the input string.
+        String input = "radar";
+
+        // Convert the string into a character array.
+        char[] chars = input.toCharArray();
+
+        // Initialize pointer at the beginning.
+        int start = 0;
+
+        // Initialize pointer at the end.
+        int end = chars.length - 1;
+
+        // Assume palindrome initially.
+        boolean isPalindrome = true;
+
+        // Continue comparison until pointers cross.
+        while (start < end) {
+
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
